a17c29ce3bb76fd6f426b71da319fe832414af05
PTFS <koha-dev@ptfs.com>
2009-05-07 09:04:55 -0400

    1-41 Added option -skipmail to overdue_notices.pl to ignore any messages that would have been e-mailed otherwise.

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -49,6 +49,7 @@ overdue_notices.pl [ -n ] [ -library <branchcode> ] [ -max <number of days> ] [ 
    -help                          brief help message
    -man                           full documentation
    -n                             No email will be sent
+   -skipmail                      Accounts with email address will be ignored.
    -max          <days>           maximum days overdue to deal with
    -library      <branchname>     only deal with overdues from this library
    -csv          <filename>       populate CSV file
@@ -228,6 +229,7 @@ my $help    = 0;
 my $man     = 0;
 my $verbose = 0;
 my $nomail  = 0;
+my $skipmail = 0;
 my $MAX     = 90;
 my $mybranch;
 my $csvfilename;
@@ -240,6 +242,7 @@ GetOptions(
     'help|?'         => \$help,
     'man'            => \$man,
     'v'              => \$verbose,
+    'skipmail'	     => \$skipmail,
     'n'              => \$nomail,
     'max=s'          => \$MAX,
     'library=s'      => \$mybranch,
@@ -455,7 +458,7 @@ END_SQL
                         }
                       );
                 } else {
-                    if ($email) {
+                    if ( $email && !$skipmail ) {
                         C4::Letters::EnqueueLetter(
                             {   letter                 => $letter,
                                 borrowernumber         => $borrowernumber,
@@ -481,7 +484,7 @@ END_SQL
                                 titles         => $titles,
                                 outputformat   => defined $csvfilename ? 'csv' : defined $htmlfilename ? 'html' : '',
                             }
-                          );
+                          ) unless $skipmail;
                     }
                 }
             }
