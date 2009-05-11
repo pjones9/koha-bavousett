ef17e94180b8d9b034949a5c6031bb9e17239e18
Colin Campbell <colin.campbell@ptfs-europe.com>
2009-05-11 13:00:59 +0100

    Add Granular permission for add bibliographic (2-1)

--- installer/data/mysql/en/mandatory/userpermissions.sql
+++ installer/data/mysql/en/mandatory/userpermissions.sql
@@ -1,6 +1,7 @@
 INSERT INTO permissions (module_bit, code, description) VALUES
    ( 1, 'circulate_remaining_permissions', 'Remaining circulation permissions'),
    ( 1, 'override_renewals', 'Override blocked renewals'),
+   (9, 'add_bibliographic', 'Create a bibliographic record'),
    (13, 'edit_news', 'Write news for the OPAC and staff interfaces'),
    (13, 'label_creator', 'Create printable labels and barcodes from catalog and patron data'),
    (13, 'edit_calendar', 'Define days when the library is closed'),
