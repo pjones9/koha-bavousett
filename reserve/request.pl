edcf61d6881f130019ef455f6ba2d8c17c32b742
Colin Campbell <colin.campbell@ptfs-europe.com>
2009-05-11 18:33:35 +0100

    Add basic holds granular permissions

--- reserve/request.pl
+++ reserve/request.pl
@@ -50,7 +50,7 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
         query           => $input,
         type            => "intranet",
         authnotrequired => 0,
-        flagsrequired   => { reserveforothers => 1 },
+        flagsrequired   => { reserveforothers => '*' },
     }
 );
 
