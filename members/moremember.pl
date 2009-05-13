615e5224f4e6c21786b95cd2e9115de8baa9ea72
David Birmingham <dbirmingham@ptfs.com>
2009-05-13 19:34:06 -0400

    Tweaks to a previous commit to toggle the display of initials and othernames.

--- members/moremember.pl
+++ members/moremember.pl
@@ -373,4 +373,6 @@ $template->param(
     "dateformat_" . (C4::Context->preference("dateformat") || '') => 1,
 );
 
+$template->param("showinitials" => C4::Context->preference('DisplayInitialsOthernames'));
+
 output_html_with_http_headers $input, $cookie, $template->output;
