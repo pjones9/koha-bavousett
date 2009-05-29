31c0a5447a97c2ccb4f77565528e0dcccc7b3d04
Jesse Weaver <pianohacker@gmail.com>
2009-05-29 10:03:03 -0600

    Bugfixing and further improvements

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -284,6 +284,7 @@ my $notforloan_label_of = get_notforloan_label_of();
 
 my $biblioLoop = [];
 my $numBibsAvailable = 0;
+my $numPolicyBlocked = 0;
 my $itemLevelTypes = C4::Context->preference('item-level_itypes');
 $template->param('item-level_itypes' => $itemLevelTypes);
 
@@ -336,7 +337,6 @@ foreach my $biblioNum (@biblionumbers) {
 
     $biblioLoopIter{itemLoop} = [];
     my $numCopiesAvailable = 0;
-    my $numPolicyBlocked = 0;
     foreach my $itemInfo (@{$biblioData->{itemInfos}}) {
         my $itemNum = $itemInfo->{itemnumber};
         my $itemLoopIter = {};
