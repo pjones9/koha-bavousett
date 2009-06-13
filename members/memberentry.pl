f8471ebe156d6b428df4a55d034b22d0496e45cc
PTFS <koha-dev@ptfs.com>
2009-06-13 15:38:05 -0400

    Memberentry fixed to work with StorePasswordPlaintext syspref.

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -102,6 +102,10 @@ foreach (@field_check) {
 $template->param("add"=>1) if ($op eq 'add');
 $template->param("checked" => 1) if (defined($nodouble) && $nodouble eq 1);
 ($borrower_data = GetMember($borrowernumber,'borrowernumber')) if ($op eq 'modify' or $op eq 'save');
+if ( C4::Context->preference('StorePasswordPlaintext') ) {
+  $borrower_data->{'password'} = $borrower_data->{'password_plaintext'};
+  warn "Password: " . $borrower_data->{'password_plaintext'};
+}
 my $categorycode  = $input->param('categorycode') || $borrower_data->{'categorycode'};
 my $category_type = $input->param('category_type');
 my $new_c_type = $category_type; #if we have input param, then we've already chosen the cat_type.
