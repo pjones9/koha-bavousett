7c3f8952525e8638bc9c140fc000af8ae603e61a
Jane Wagner <jwagner@ptfs.com>
2009-06-15 11:52:53 -0400

    Revision to holds to pull, change default date range to yesterday-today
    Instead of 10 years ago to yesterday, change default date range to
    only yesterday and today

--- circ/pendingreserves.pl
+++ circ/pendingreserves.pl
@@ -66,9 +66,8 @@ my $author;
 my ( $year, $month, $day ) = Today();
 my $todaysdate     = sprintf("%-04.4d-%-02.2d-%02.2d", $year, $month, $day);
 my $yesterdaysdate = sprintf("%-04.4d-%-02.2d-%02.2d", Add_Delta_YMD($year, $month, $day,   0, 0, -1));
-#changed from delivered range of 10 years-yesterday to 2 days ago-today
-# Find two days ago for the default shelf pull start and end dates
-my $pastdate       = sprintf("%-04.4d-%-02.2d-%02.2d", Add_Delta_YMD($year, $month, $day, 0, 0, -2));
+#changed from delivered range of 10 years-yesterday to 1 days ago-today
+my $pastdate = $yesterdaysdate;
 
 #		Predefine the start and end dates if they are not already defined
 $startdate =~ s/^\s+//;
